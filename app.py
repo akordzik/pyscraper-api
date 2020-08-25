c18d60f004e14cc92840150b217de66e93974e09
Artur Kordzik <akordzik@outlook.com>
2020-08-25 22:42:05 +0200

    added mongo and basic auth

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
+import os
+import json
+
+from bson import json_util
 from flask import Flask
+from pymongo.mongo_client import MongoClient
+from flask_basicauth import BasicAuth
+
 app = Flask(__name__)
 
+client = MongoClient(os.environ['MONGOLAB_URI'])
+db = client.get_default_database()
+collection = db['advertisements']
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+class CustomAuth(BasicAuth):
+    def check_credentials(_, username: str, password: str):
+        return username == os.environ['USERNAME'] and password == os.environ['PASSWORD']
+
+
+app.config['BASIC_AUTH_FORCE'] = True
+basic_auth = CustomAuth(app)
+
+
+@app.route('/adverts/')
+def get():
+    adverts = list(collection.find())
+    return json.dumps(adverts, default=json_util.default, ensure_ascii=False).encode('utf8')
